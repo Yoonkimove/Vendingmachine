57094ba569d3b27d68bae117051002331b159d20
Yoonkyung Kim <yoonkimove@gmail.com>
2018-08-28 12:52:31 +0900

    Add milk, milkcoffee, return changes

--- vm.py
+++ vm.py
@@ -4,7 +4,7 @@ class VendingMachine:
 
     def run(self, raw):
         tokens = raw.split(" ")
-        cmd, params = tokens[0], tokens[1:]
+        cmd, params = tokens[0], tokens[1:]   # ex)["동전","100","잔액"]
 
         if cmd == "잔액":
             return "잔액은 " + str(self._change) + "원입니다"
@@ -18,16 +18,33 @@ class VendingMachine:
             return coin + "원을 넣었습니다"
 
         elif cmd == "음료":
-            price = 150
+            prices = {'커피': 150, '우유': 200, '밀크커피': 300}
             beverage = params[0]
-            known_beverage = "커피"
-            if beverage != known_beverage:
+            known_beverages = list(prices.keys())
+            if beverage not in known_beverages:
                 return "알 수 없는 음료입니다."
-            if self._change < price:
+            if self._change < prices[beverage]:
                 return "잔액이 부족합니다."
 
-            self._change = self._change - price
+            self._change = self._change - prices[beverage]
             return beverage + "가 나왔습니다."
+
+        elif cmd == "반환":
+            coins = [500,100,50,10]
+            changes = ""
+            i = 0
+            if self._change == 0:
+                changes = "잔액이 없습니다.."
+            while self._change > 0:
+                if self._change // coins[i] > 0:
+                    changes = changes + str(coins[i]) + ","
+                    self._change = self._change - coins[i]
+                else:
+                    i = i+1
+            changes = changes[:-1]
+            return changes
+
+
 
         # elif cmd == "음료":
         #     drink = params[0]
